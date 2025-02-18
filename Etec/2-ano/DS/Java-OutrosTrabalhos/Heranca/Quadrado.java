d18fc7ed2561cdd150a798012eabe738b8378a72
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2025-02-18 08:43:52 -0300

    Create Quadrado.java

---
+++ Etec/2-ano/DS/Java-OutrosTrabalhos/Heranca/Quadrado.java
@@ -0,0 +1,12 @@
+public class Figura {
+    public String cor;
+    //Declara a variavel cor
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    //Configura a variavel cor para receber o seu valor
+    public String getCor () {
+        return cor;
+    }
+    //Retorna a variavel cor ao seu valor normal
+}
