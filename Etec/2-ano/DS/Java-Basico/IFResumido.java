663dbe1145b5a10e04446753a2d5b137d8277fd2
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2025-02-18 07:50:29 -0300

    Create IFResumido.java

---
+++ Etec/2-ano/DS/Java-Basico/IFResumido.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+public class IfResumido {
+    public static void main(String[] args) {
+        int a = 10, b = 15, maior;
+        if (a > b) {
+            maior = a;
+        } else {
+            maior = b;
+        }
+        JOptionPane.showMessageDialog(null, "Usando um if comun: " + maior);
+
+        int c = 10, d = 8;
+        maior = (c > d) ? c : d;
+        JOptionPane.showMessageDialog(null, "Usando um if resumido: " + maior);
+    }
+}
